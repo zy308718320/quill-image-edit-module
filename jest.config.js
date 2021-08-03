246c2359b43c02926aa82312318c8c5f483a9971
evanzhou(周仪) <evanzhou@tencent.com>
2021-08-03 15:14:50 +0800

    feat: 调整测试覆盖率目录位置

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
     '^.+\\.svg$': '<rootDir>/src/tests/mocks/svgFileMock.ts',
   },
   testEnvironment: 'jsdom',
+  coverageDirectory: '<rootDir>/tests/coverage',
   collectCoverage: true, // 统计覆盖率
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
